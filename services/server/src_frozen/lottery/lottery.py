a5d3c6572a9c4e51ac638bc4b0b1f2652e032843
mario <hernanmlagarde@gmail.com>
2026-09-10 08:51:09 -0300

    check file bytes

--- services/server/src_frozen/lottery/lottery.py
+++ services/server/src_frozen/lottery/lottery.py
@@ -1,4 +1,6 @@
 import csv
+import io
+import os
 from collections.abc import Iterator
 from .bet import Bet
 
@@ -13,30 +15,31 @@ class Lottery:
         return bet.number == _LOTTERY_WINNER_NUMBER
 
     def store_bets(self, bets: list[Bet]) -> None:
-        with open(self.storage_path, "a+") as file:
-            writer = csv.writer(file, quoting=csv.QUOTE_MINIMAL)
+        with open(self.storage_path, "ab") as file:
             for bet in bets:
-                writer.writerow(
-                    [
-                        bet.agency_id,
-                        bet.first_name,
-                        bet.last_name,
-                        bet.document,
-                        bet.birthdate,
-                        bet.number,
-                    ]
-                )
+                row = io.StringIO(newline="")
+                csv.writer(row, quoting=csv.QUOTE_MINIMAL).writerow([
+                    bet.agency_id, bet.first_name, bet.last_name, bet.document,
+                    bet.birthdate, bet.number,
+                ])
+                data = row.getvalue().encode("utf-8")
+                if file.write(data) != len(data):
+                    raise OSError("bad write file")
 
     def load_bets(self) -> Iterator[Bet]:
-        with open(self.storage_path, "r") as file:
-            reader = csv.reader(file, quoting=csv.QUOTE_MINIMAL)
-            for row in reader:
-                [agency_id, first_name, last_name, document, birthdate, number] = row
-                yield Bet(
-                    int(agency_id),
-                    first_name,
-                    last_name,
-                    int(document),
-                    birthdate,
-                    int(number),
-                )
+        with open(self.storage_path, "rb") as file:
+            expected_bytes = os.fstat(file.fileno()).st_size
+            data = file.read(expected_bytes)
+            if len(data) != expected_bytes:
+                raise OSError("bad read file")
+
+        reader = csv.reader(
+            io.StringIO(data.decode("utf-8"), newline=""),
+            quoting=csv.QUOTE_MINIMAL,
+        )
+        for row in reader:
+            [agency_id, first_name, last_name, document, birthdate, number] = row
+            yield Bet(
+                int(agency_id), first_name, last_name, int(document), birthdate,
+                int(number),
+            )
